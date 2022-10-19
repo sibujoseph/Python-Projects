669ebfee53296d21d9549a0dde7b1fe0c5a5e43e
~sj <sibu.joseph@mail.com>
2022-10-18 23:28:58 -0500

    Common Functions

---
+++ Python/common_functions.py
@@ -0,0 +1,39 @@
+# common functions
+import sys
+import logging
+
+def strip_comments(code, SQL_or_PY, Multi_or_Single, Exclude_or_Include, look_for_start, look_for_end=''):
+    " Doc Removes comments from code "
+    this_method="strip_comments"
+
+    code = code
+    temp_code = ''
+    start_pos_init = 0
+    start_pos = 0
+    end_pos = 0
+    NEW_LINE = '\n'
+
+    look_for_end = NEW_LINE if look_for_end=='' else look_for_end
+
+    if Multi_or_Single == 'M' and SQL_or_PY='='SQL':
+        while(look_for_start in code or look_for_end in code):
+            start_pos=(code.find(look_for_start, start_pos_init, len(code)))
+            end_pos=(code.find(look_for_start, start_pos, len(code)))
+            code=code[:start_pos]+code[end_pos+len(look_for_end)]
+            if start_pos in (0, -1):
+                break
+    
+    if Multi_or_Single=='S' and SQL_or_PY in ('SQL','PY'):
+        if Exclude_or_Include=='E':
+            for eachline in code.splitlines(True):
+                start_pos=(eachline.find(look_for_start, start_pos_init, len(eachline)))
+                temp_code=temp_code+eachline[:start_pos]+NEW_LINE
+        else:
+            start_pos=(code.find(look_for_start, start_pos_init, len(code)))
+            end_pos=(code.find(look_for_end, start_pos+len(look_for_start), len(code)))
+            temp_code=code[start_pos+len(look_for_start):end_pos]
+        code=temp_code
+    
+    return code
+
+
