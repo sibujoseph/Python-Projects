3c47ef0a6361f8843acfe59940a0ebc9043e6bae
sibujoseph <40472682+sibujoseph@users.noreply.github.com>
2025-09-24 01:07:57 -0500

    Update type-annotations.py

--- Python/type-annotations.py
+++ Python/type-annotations.py
@@ -1,4 +1,7 @@
-# for constants 
+# for constants, use Final as it will ensure 
+# repeat declarations of the same constants 
+# are warned in IDE
+
 from typing import Final
 
 PI_VALUE: Final(float) = 3.14
